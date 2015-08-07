26949e5676d059711214db0d443fdf62940a13cd
gaurang-agarwal <er.gaurangagarwal@yahoo.com>
2015-08-07 15:53:13 +0530

    Running Code
    Running Code

--- ConcurrenyInJava/src/filesearch/FileSearcher.java
+++ ConcurrenyInJava/src/filesearch/FileSearcher.java
@@ -1,73 +1,101 @@
 package filesearch;
-
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
-
 public class FileSearcher {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-Producer producer = new Producer("/home/gaurang/workspace/");
+		Producer producer = new Producer("/home/appnetix/cvs/");
 		Thread thread= new Thread(producer);
 		thread.start();
+		TimeUnit.SECONDS.sleep(5);
+		producer.setForceStop(true);
 		
 	}
-	
 }
-
-
 class Producer implements Runnable{
-	BlockingQueue<File> queue= new ArrayBlockingQueue<File>(1024);
-	 String rootPath;
+	BlockingQueue<File> queue= new ArrayBlockingQueue<File>(10024);
+	String rootPath;
+	private volatile boolean forceStop=false;
+	public void setForceStop(boolean forceStop) {
+		this.forceStop = forceStop;
+	}
+	public boolean getForceStop() {
+		return forceStop;
+	}
 	public Producer(String rootPath) {
 		// TODO Auto-generated constructor stub
-	this.rootPath=rootPath;
+		this.rootPath=rootPath;
 	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-	Consumer consumer = new Consumer(queue);	
-	Thread consumerThread= new Thread(consumer);
+		Consumer consumer = new Consumer(queue);
+		Thread consumerThread= new Thread(consumer);
 		consumerThread.start();
-		System.out.println("><Consumer Started<<<<<<<<<<");
-	File file = new File(rootPath);
-	produce(file);
-	try {
-		Thread.sleep(5000);
+		System.out.println(">>>>>>>>>>>Consumer Started<<<<<<<<<<");
+		File file = new File(rootPath);
 		
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	System.out.println(">>>>>>>>consumer>>>>>>>>>>"+consumer.getFileList());
-	
-}
-
-private void produce(File file) {
-		// TODO Auto-generated method stub
-		if(file.isDirectory()){
-			queue.add(file);
-			for(File filelist:file.listFiles()){
-				if(filelist.isDirectory()){
-					produce(filelist);
-				}
-			}
-		
+		try {
+			produce(file);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			System.out.println(">>>>Produce Stopped Forcefully<<<<<<");
 		}
 		
 		
+		try {
+			System.out.println(">>>>>>>Produce Completed<<<<<<<<<<<<<<<");
+			consumerThread.join();
+			FileWriter writer = new FileWriter("/home/appnetix/Desktop/wring.txt");
+				writer.write(consumer.getFileList()+"");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) 
+		{
+			// TODO Auto-generated catch block
+			System.out.println(">>>>>>>>>>Producer Interrupted<<<<<<<<<");
+			consumerThread.interrupt();
+		}
+		finally{
+			System.out.println(">>>>>>In Finally<<<<<<<");
+		}
 		
+	}
+	
+	List <String> fileName= new ArrayList<String>();
+	public List<String> getFileList(){
+		return fileName;
+	}
+	public void  setFileList(List<String> fileName){
+		this.fileName=fileName;
+	}
+	
+	private void produce(File file) throws Exception {
+		// TODO Auto-generated method stub
+		if(getForceStop()){
+			throw new Exception();
+		}
+		if(file.isDirectory()){
+			queue.offer(file);
+			for(File filelist:file.listFiles(new MyFileFilter())){
+					produce(filelist);
+			}
+		}
 	}
 }
 class Consumer implements Runnable{
-
 	BlockingQueue<File> queue=null;
-	
+	private volatile boolean forceStop=false;
 	public Consumer(BlockingQueue<File> queue) {
 		// TODO Auto-generated constructor stub
 		this.queue=queue;
@@ -77,42 +105,56 @@ class Consumer implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		try {
-			Thread.sleep(100);
-			while(queue.size()>0){
-			File directory=queue.take();
-			String [] fileList=directory.list(new MyFileFilter(".java"));
-			for(String file:fileList){
-				fileName.add(file);
-			}
+			
+			while(true){
+				File directory=queue.poll(5,TimeUnit.SECONDS);
+				if(directory!=null){
+				String [] fileList=directory.list(new MyFileNameFilter(".java"));
+				for(String file:fileList){
+					fileName.add(file+"\n");
+				}
+				}
+				else {
+					System.out.println("queue>>>>>>>>>"+queue);
+					break;
+				}
 			}
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
+			System.out.println(">>>Consumer Thread Interrupted<<<");
 		}
-		
-		
-		
 	}
-	
-	public List<String> getFileList(){
+public List<String> getFileList(){
 		return fileName;
 	}
-	
+
+public void setForceStop(boolean forceStop){
+	this.forceStop=forceStop;
 }
 
-class MyFileFilter implements FilenameFilter{
-String pattern;
-	MyFileFilter(String pattern) {
+}
+class MyFileNameFilter implements FilenameFilter{
+	String pattern;
+	MyFileNameFilter(String pattern) {
 		// TODO Auto-generated method stub
-this.pattern=pattern;
-		
+		this.pattern=pattern;
 	}
-	
 	@Override
 	public boolean accept(File dir, String name) {
 		// TODO Auto-generated method stub
 		return name.endsWith(pattern);
 	}
-	
 }
 
+
+class MyFileFilter implements FileFilter{
+
+	@Override
+	public boolean accept(File pathname) {
+		// TODO Auto-generated method stub
+		return pathname.isDirectory();
+		
+	}
+	
+}
