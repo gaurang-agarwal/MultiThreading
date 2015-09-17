5ac2d98d85048350c5b9e1a76bcd01c56b0a3b51
gaurang <gaurang@Light-PC>
2015-09-18 00:12:13 +0530

    1.1

--- DS/src/pack/InsertionSort.java
+++ DS/src/pack/InsertionSort.java
@@ -6,8 +6,8 @@ public class InsertionSort {
 
 public static void main(String [] args){
 	int a[]={4,1,2,5,3};
-	new InsertionSort().oppSort(a);
-	System.out.println("\t\tResult:::"+Arrays.toString(a));
+	new InsertionSort().sort(a);
+	System.out.println("\n\nResult:::"+Arrays.toString(a));
 	//new InsertionSort().sort(a);
 	//System.out.println("\t\tResult:::"+Arrays.toString(a));
 	
@@ -17,16 +17,13 @@ void sort(int a[]){
 	int j=0;
 	int temp;
 	for(int i=1;i<a.length;i++){
-		j=0;
+		j=i-1;
 		temp=a[i];
-		while(j<i&&temp>a[j]){
-			j++;
+		while(j>=0&&temp<a[j]){
+			a[j+1]=a[j--];
 		}
-		System.out.println("a[i]:"+a[i]+"  "+"i:"+i+"  "+"j:"+j);
-		System.out.println("Before shiting :"+Arrays.toString(a));
-		rightShift(a, i, j);
-		System.out.println("After shiting :"+Arrays.toString(a));
-	a[j]=temp;
+		
+	a[j+1]=temp;
 	
 }
 }
